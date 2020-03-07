8bd7fb965e71c723d9e09ab301cd4802bf92967f
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-06 21:16:36 -0800

    Update configurations.py

--- structure/master/configurations.py
+++ structure/master/configurations.py
@@ -10,3 +10,10 @@ class ProductionConfig(BaseCongig):
 	Production specific config
 	"""
 	DEBUG = False
+
+class DevelopmentConfig(BaseCongig):
+	"""
+	Development environment specific configuration
+	"""
+	DEBUG = True
+	TESTING = True
